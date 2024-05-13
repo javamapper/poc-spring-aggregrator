096b383864e0bfbe8feab1b0f61a613fd3ce7be9
javamapper <69502340+javamapper@users.noreply.github.com>
2024-05-13 23:23:36 +0530

    initial commit

---
+++ microservice-accounting-service/src/main/java/com/javamapper/AccountingServiceApplication.java
@@ -0,0 +1,26 @@
+/*
+ * Coding practice lab
+ *
+ * Website Url: https://github.com/javamapper/io
+ * Base Repo Url: https://github.com/javamapper
+ * Linkedin profile: https://www.linkedin.com/in/anilref12/
+ * 
+ * v1.0
+ *
+ * 2024-03-18
+ * 
+ * This file is copyrighted - 
+ *
+ * Please appreciate if this put value in your knowledge and work
+ * 
+*/
+package com.javamapper;
+
+public class AccountingServiceApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
